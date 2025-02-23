baef2fc51a19671b985677015b56962c0588554d
Outsider1010 <114062925+Outsider1010@users.noreply.github.com>
2025-02-23 23:16:58 +0100

    simplified useless attributes and cleaned the main

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from src.ShapeAnalogyApp import App
 
-app = App()
-"""SA = PixelShape(img="resources/ellipse_1.bmp")
-SB = PixelShape(img="resources/ellipse_2.bmp")
-SC = PixelShape(img="resources/ellipse_3.bmp")"""
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
