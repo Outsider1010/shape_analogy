537083539b4f558ebbf00fc43780192d64b8ef8c
alexis <alexischapusot@gmail.com>
2025-03-18 21:22:51 +0100

    ajout des strategies dans la fenêtre, epsilon et debut keep

--- src/view/OptionView.py
+++ src/view/OptionView.py
@@ -1,4 +1,4 @@
-from tkinter import ttk,Toplevel,Label,Button
+from tkinter import ttk,Toplevel,Label,Button,Spinbox,StringVar
 from src.ShapeAnalogyModel import ShapeAnalogyModel
 from PIL import Image, ImageTk,ImageTk,ImageOps
 from src.birectangle.cuttingmethod.CuttingIn8Method import CuttingIn8Method
@@ -10,42 +10,44 @@ from src.birectangle.birectangleanalogy.ExtSigmoidAnalogy import ExtSigmoidAnalo
 from src.birectangle.birectangleanalogy.SigmoidCenterAnalogy import SigmoidCenterAnalogy
 from src.birectangle.birectangleanalogy.ExtSigmoidAnalogy import ExtSigmoidAnalogy
 from src.birectangle.cuttingmethod.FirstCuttingIn4Method import FirstCuttingIn4Method
-from src.birectangle.innerrectanglefinder.InnerRectangleFinder import InnerRectangleFinder
+from src.birectangle.birectangleanalogy.CornerSigmoidAnalogy import CornerAnalogy
+from src.birectangle.cuttingmethod.FullHorizontalCut import FullHorizontalCut
+from src.birectangle.cuttingmethod.FullVerticalCut import FullVerticalCut
+from src.birectangle.cuttingmethod.FullSideNonDisjointCut import FullSideNonDisjointCut
 from src.birectangle.innerrectanglefinder.LargestRectangleFinder import LargestRectangleFinder
+from src.birectangle.birectangleanalogy.BiSegmentAnalogy import BiSegmentAnalogy
 # Define the classes representing the options
-class BiRectangleOption(ttk.Frame):
+class BiRectangleOption():
 
     def __init__(self,root,model:ShapeAnalogyModel):
-        super().__init__(root)
         self.model = model
         self.root = root
         self.windows = None
         self.birectangleAnalogyStrategy = {
-            "Sigmoid center":SigmoidCenterAnalogy(),
-            "Extended sigmoid":ExtSigmoidAnalogy(),
-            "BiSegment Analogy":BiSegmentAnalogy(),
-            "CornerSigmoidAnalogy":CornerAnalogy()
+            "Sigmoid center":SigmoidCenterAnalogy,
+            "Extended sigmoid":ExtSigmoidAnalogy,
+            "BiSegment Analogy":BiSegmentAnalogy,
+            "CornerSigmoidAnalogy":CornerAnalogy
         }
         self.cuttingStrategy = {
-            "Cut4":FirstCuttingIn4Method(),
-            "Cut8":CuttingIn8Method(),
-            "Horizontal cut":FullHorizontalCut(),
-            "Vertical cut": FullVerticalCut(),
-            "Non disjoint cut":FullSideNonDisjointCut(),
+            "Cut4":FirstCuttingIn4Method,
+            "Cut8":CuttingIn8Method,
+            "Horizontal cut":FullHorizontalCut,
+            "Vertical cut": FullVerticalCut,
+            "Non disjoint cut":FullSideNonDisjointCut,
             
         }
         self.innerRectangleFinderStrategy = {
-            "Largest rectangle": LargestRectangleFinder()
+            "Largest rectangle": LargestRectangleFinder,
         }
-        self.pointAnalogyStraty = {
-            "Arithmetic analogy": ArithmeticPointAnalogy()
+        self.pointAnalogyStrategy = {
+            "Arithmetic analogy": ArithmeticPointAnalogy,
         }
-        self.rectangleAnalogy = {
-            "Center dimension analogy": CenterDimAnalogy(),
-            "Top left dimension analogy": TopLeftDimAnalogy()
+        self.rectangleAnalogyStrategy = {
+            "Center dimension analogy": CenterDimAnalogy,
+            "Top left dimension analogy": TopLeftDimAnalogy,
         }
-        self.birect_analogy_combo = ttk.Combobox(self, values=self.birectangleAnalogyStrategy, state="readonly")
-        self.birect_analogy_combo.set()
+        
         """self.birect_cutting_combo = ttk.Combobox(self,values=self.model.get_birectangle_cutting_strategy(),state="readonly")
         self.birect_cutting_combo.set(self.model.get_birectangle_cutting_strategy_values())
         self.birect_inner_rectangle_finder = ttk.Combobox(self,values=self.model.get_birectangle_inner_rectangle_finder_strategy(),state="readonly")
@@ -56,7 +58,8 @@ class BiRectangleOption(ttk.Frame):
 
     def show(self):
         self.windows = Toplevel(self.root)
-        self.pack(side="top")
+        self.setupWindows()
+
     """self.birect_inner_rectangle_finder.grid(column=2,row=0)
         self.birect_cutting_combo.grid(column=1,row=0,padx=20)
         self.birect_analogy_combo.grid(column=0,row=0,padx=20)"""
@@ -74,7 +77,81 @@ class BiRectangleOption(ttk.Frame):
 
     def select_inner_rectangle_finder_strategy(self,event):
         self.model.set_birectangle_inner_rectangle_finder_strategy(self.birect_inner_rectangle_finder.get())
-
+    def setupWindows(self):
+        keep = StringVar()
+        self.windows.grid_rowconfigure(0, weight=1)
+        self.windows.grid_rowconfigure(1,weight=2)
+        self.windows.grid_columnconfigure(0, weight=1)
+        
+        frameStrategy = ttk.Frame(self.windows)
+        frameStrategy.grid(column=0, row=0, sticky="ew")
+        frameStrategy.grid_columnconfigure(0, weight=1)
+        frameStrategy.grid_columnconfigure(1, weight=1)
+        frameStrategy.grid_columnconfigure(2, weight=1)
+        
+        strategiesLabel = Label(frameStrategy, text="Strategy")
+        strategiesLabel.grid(column=1, row=0, sticky="ew", pady=20)
+        
+        #birectangleAnalogy
+        birect_analogy_combo = ttk.Combobox(
+            frameStrategy, 
+            values=list(self.birectangleAnalogyStrategy.keys()), 
+            state="readonly"
+        )
+        birect_analogy_combo.set(
+            list(self.birectangleAnalogyStrategy.keys())[
+                list(self.birectangleAnalogyStrategy.values()).index(
+                    self.model.getMethod().getBirectangleAnalogy().__class__
+                )
+            ]
+        )
+        birect_analogy_combo.grid(column=0, row=2, sticky="ew")
+        labelBirectangle = Label(frameStrategy, text="Birectangle analogy")
+        labelBirectangle.grid(column=0, row=1, sticky="ew")
+        
+        #cuttingMethod
+        cuttingLabel = Label(frameStrategy,text="Cutting method")
+        birect_cutting_combo = ttk.Combobox(frameStrategy,values=list(self.cuttingStrategy.keys()),state="readonly")
+        birect_cutting_combo.set(list(self.cuttingStrategy.keys())[
+                list(self.cuttingStrategy.values()).index(
+                    self.model.getMethod().getCuttingMethod().__class__
+                )
+            ])
+        birect_cutting_combo.grid(column=1,row=2)
+        cuttingLabel.grid(column=1,row=1)
+        
+        
+        #innerRectangleFinderMethod
+        innerRectangleLabel = Label(frameStrategy,text="Inner rectangle finder method")
+        inner_rectangle_finder_combo = ttk.Combobox(frameStrategy,values=list(self.innerRectangleFinderStrategy.keys()),state="readonly")
+        inner_rectangle_finder_combo.set(list(self.innerRectangleFinderStrategy.keys())[
+                list(self.innerRectangleFinderStrategy.values()).index(
+                    self.model.getMethod().getInnerRectangleFinder().__class__
+                )
+            ])
+        inner_rectangle_finder_combo.grid(column=2,row=2)
+        innerRectangleLabel.grid(column=2,row=1)
+        parametersFrame  = ttk.Frame(self.windows)
+        parametersFrame.grid(column=0, row=1, sticky="ew")
+        parametersFrame.grid_columnconfigure(0, weight=1)
+        parametersFrame.grid_columnconfigure(1, weight=1)
+        parametersFrame.grid_columnconfigure(2, weight=1)
+      
+        parameterLabel = Label(parametersFrame,text="parameters")
+      
+        epsilonLabel = Label(parametersFrame,text="epsilon")
+        #Set the default value for SpinBox
+        my_var= StringVar(parametersFrame)
+        my_var.set(self.model.getMethod().getEpsilon())
+        epsilonBox = Spinbox(parametersFrame, from_=0.01, to=0.45,increment=0.05,textvariable=my_var)
+      
+        parameterLabel.grid(column=1,row=0,sticky="ew")
+        epsilonLabel.grid(column=0,row=0)
+        epsilonBox.grid(column=0,row=1)
+        
+        #keep
+        keepFrame = ttk.Frame(parametersFrame)
+        keepLabel = Label(keepFrame,text="keep")
 class TomographyOption(ttk.Frame):
     def __init__(self, root,model:ShapeAnalogyModel):
         super().__init__(root)
