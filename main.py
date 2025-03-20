f1477f05591e89cee54467a17b64528f5c4c7ccc
Tompotio <atolenatsu@gmail.com>
2025-03-20 10:44:02 +0100

    Pas important

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from src.birectangle.bi_rectangle_method import BiRectangleMethod
 from src.shapes.pixel_shape import PixelShape
 
-SA = PixelShape(img="resources/circle_1.bmp")
-SB = PixelShape(img="resources/circle_2.bmp")
-SC = PixelShape(img="resources/circle_1.bmp")
+SA = PixelShape(img="resources/circle_4.bmp")
+SB = PixelShape(img="resources/circle_1.bmp")
+SC = PixelShape(img="resources/circle_3.bmp")
 
-m = BiRectangleMethod(algo='rec', keep=2, epsilon=0.001, maxDepth=10)
+m = BiRectangleMethod(algo='iter', plot='step', keep=1, epsilon=0.0001)
 m.analogy(SA, SB, SC)
